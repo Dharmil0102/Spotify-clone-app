7299609328ef84b607285de7401d928bed6dca29
Dharmil0102 <dharmil.mehta20@gmail.com>
2024-05-09 14:09:04 +0530

    changed the client_id in login.jsx

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { StateProvider } from './utils/StateProvider';
+import reducer, { initialState } from './utils/reducer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
